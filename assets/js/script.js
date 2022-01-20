7a1cb7a9059891bb2d363c1a79cd8de4c6155f03
mitzusca <mihai_93_hd@yahoo.com>
2022-01-20 02:25:19 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswers() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubstractQuestion() {
+
+}
+function displayMultiplyQuestion () {
+
+}
+function displayDivideQuestion () {
+    
+}
