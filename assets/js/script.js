83ad994a96f77adedac2368d7b2ad8f95884e04b
mitzusca <mihai_93_hd@yahoo.com>
2022-01-23 07:59:56 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === 'substract'){
         displaySubstractQuestion(num1, num2);
+    } else if(gameType === 'division'){
+        displayDivideQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -76,6 +78,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-') {
         return [operand1 - operand2, 'substract'];
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -111,6 +115,8 @@ function displayMultiplyQuestion (operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
 }
-function displayDivideQuestion () {
-
+function displayDivideQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
 }
